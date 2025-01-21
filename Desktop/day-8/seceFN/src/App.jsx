8710e17ad55032565e585db1342dc0cc2f5b6f13
DarsanV <darsan.v27@gmail.com>
2025-01-22 00:20:52 +0530

    Initial commit

---
+++ Desktop/day-8/seceFN/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Content from './components/Content.jsx'
+import Title from './components/title.jsx'
+import Profile from "./components/Profile.jsx";
+import Skills from "./components/skills.jsx";
+function App() {
+  return (
+    <>
+    <Title/>
+      <h1 id="nam"> HI, I'M DARSAN </h1>
+      <Content />
+      <Profile/>
+      <Skills/>
+    </>
+  )
+}
+
+export default App
---
+++ Desktop/day-8/seceFN/src/components/Content.jsx
@@ -0,0 +1,9 @@
+
+import "./Content.css"
+function Content() {
+    return (
+       <h2 id="con">DEVOPS ENGINEER... </h2>
+    )
+}
+
+export default Content
